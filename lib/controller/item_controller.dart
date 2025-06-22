91e9489cf719fc22817e637c149fc2b3d876c00a
jackkslash <jackfaulknerbis@hotmail.co.uk>
2025-06-22 14:23:43 +0100

    Item operations

---
+++ lib/controller/item_controller.dart
@@ -0,0 +1,59 @@
+import 'package:kaloria/models/item.dart';
+import 'package:kaloria/controller/db_controller.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'item_controller.g.dart';
+
+@Riverpod(keepAlive: true)
+class ItemEntriesNotifier extends _$ItemEntriesNotifier {
+  @override
+  List<ItemEntry> build() => [];
+
+  /// Loads all items for a given diary entry id
+  Future<void> loadItemsForDiary(int diaryEntryId) async {
+    final db = ref.read(databaseNotifierProvider).database;
+    if (db != null) {
+      final dbItems = await (db.select(
+        db.databaseItemEntry,
+      )..where((tbl) => tbl.diaryEntryId.equals(diaryEntryId))).get();
+      state = dbItems.map(ItemEntry.fromData).toList();
+    }
+  }
+
+  /// Adds a new item
+  Future<void> addItem(ItemEntry item) async {
+    final db = ref.read(databaseNotifierProvider).database;
+    if (db != null) {
+      final id = await db
+          .into(db.databaseItemEntry)
+          .insert(item.toCompanion(includeId: false));
+      final newItem = item.copyWith(id: id);
+      state = [...state, newItem];
+    }
+  }
+
+  /// Updates an existing item
+  Future<void> updateItem(ItemEntry item) async {
+    final db = ref.read(databaseNotifierProvider).database;
+    if (db != null) {
+      await (db.update(db.databaseItemEntry)
+            ..where((tbl) => tbl.id.equals(item.id)))
+          .write(item.toCompanion(includeId: true));
+      state = [
+        for (final i in state)
+          if (i.id == item.id) item else i,
+      ];
+    }
+  }
+
+  /// Deletes an item by id
+  Future<void> deleteItem(int id) async {
+    final db = ref.read(databaseNotifierProvider).database;
+    if (db != null) {
+      await (db.delete(
+        db.databaseItemEntry,
+      )..where((tbl) => tbl.id.equals(id))).go();
+      state = state.where((i) => i.id != id).toList();
+    }
+  }
+}
---
+++ lib/controller/item_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'item_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$itemEntriesNotifierHash() =>
+    r'8e0139eab63d80126913fcf945d1cf23109d1e22';
+
+/// See also [ItemEntriesNotifier].
+@ProviderFor(ItemEntriesNotifier)
+final itemEntriesNotifierProvider =
+    NotifierProvider<ItemEntriesNotifier, List<ItemEntry>>.internal(
+      ItemEntriesNotifier.new,
+      name: r'itemEntriesNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$itemEntriesNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$ItemEntriesNotifier = Notifier<List<ItemEntry>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
